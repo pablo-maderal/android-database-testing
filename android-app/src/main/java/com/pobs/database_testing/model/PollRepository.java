2a1e03a02629daa49963744aa463b517cbb4a111
pablo-maderal <109625588+pablo-maderal@users.noreply.github.com>
2022-07-19 20:44:17 -0400

    Delete android-app directory

--- android-app/src/main/java/com/pobs/database_testing/model/PollRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pobs.database_testing.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PollRepository extends CrudRepository<Poll, Integer> {
-}
